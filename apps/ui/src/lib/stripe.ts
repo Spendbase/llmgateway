d266688938e04f32654958a2f5db8c42663cf421
Andrii Prokhorovych <prohorovychua@gmail.com>
2026-01-13 21:35:42 +0200

    fix(ui): stripe pk

--- apps/ui/src/lib/stripe.ts
+++ apps/ui/src/lib/stripe.ts
@@ -11,7 +11,7 @@ function getStripePromise() {
 		stripePromise = loadStripe(
 			process.env.NODE_ENV === "development"
 				? "pk_test_51RRXM1CYKGHizcWTfXxFSEzN8gsUQkg2efi2FN5KO2M2hxdV9QPCjeZMPaZQHSAatxpK9wDcSeilyYU14gz2qA2p00R4q5xU1R"
-				: "pk_live_51RRXLsEAkKxa3kRayCPr9oW8dUp7mIzwev1FVpM3jpKU3StLaaiKvXCEPkewabL5hRip4IXLzFlFTLC4RpFWRknN00lX2vgZHP",
+				: "pk_live_51S2os2Cd8RUVHACGBUcOWHMnsYDREtoYiuxd5eY1NiBQLCSkdBnOUFzGcQXsgGYDYnHacT4QEWhpPS0j84lVb8JO00HuFkDgrq",
 		);
 	}
 	return stripePromise;
