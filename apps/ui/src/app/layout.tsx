a4a7c4b5fd6fb776ff80c7de972168d5734ec7b6
Алексей Андрюшин <aleksroebuck@gmail.com>
2026-02-06 14:25:09 +0200

    fix: change from third party lib to native script

--- apps/ui/src/app/layout.tsx
+++ apps/ui/src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 
@@ -56,10 +55,27 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 
 	return (
 		<html lang="en" suppressHydrationWarning>
+			<Script id="gtm-script" strategy="afterInteractive">
+				{`
+					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer', '${config.gtmId}');
+				`}
+			</Script>
 			<body
 				className={`${inter.variable} ${geistMono.variable} min-h-screen antialiased`}
 			>
-				<GoogleTagManager gtmId={config.gtmId!} />
+				<noscript>
+					<iframe
+						sandbox=""
+						src={`https://www.googletagmanager.com/ns.html?id=${config.gtmId!}`}
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+					/>
+				</noscript>
 				<Providers config={config}>{children}</Providers>
 				<Script
 					id="hs-script-loader"
