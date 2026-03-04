3b2efbfe46574668d18517f3f2474b47d6c799c7
Karen Behlarian <142582247+N3m0o@users.noreply.github.com>
2026-03-04 12:42:10 +0200

    fix(db): fix migration history (#86)
    * fix(db): fix migration history
    
    * Delete packages/db/migrations/1772616086_tired_gauntlet.sql

---
+++ packages/db/migrations/1772619744_illegal_the_renegades.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "api_key" ADD COLUMN "reset_period" text DEFAULT 'none' NOT NULL CHECK ("reset_period" IN ('daily', 'weekly', 'monthly', 'none'));--> statement-breakpoint
+ALTER TABLE "api_key" ADD COLUMN "last_reset_at" timestamptz;--> statement-breakpoint
+ALTER TABLE "api_key" ADD COLUMN "next_reset_at" timestamptz;--> statement-breakpoint
+ALTER TABLE "api_key" ADD COLUMN "expires_at" timestamptz;--> statement-breakpoint
+
+
+CREATE INDEX "api_key_next_reset_at_idx" ON "api_key" ("next_reset_at");--> statement-breakpoint
+CREATE INDEX "api_key_expires_at_idx" ON "api_key" ("expires_at");--> statement-breakpoint
